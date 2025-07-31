eff56c40165cd9bc9c6f1315bca6fc8171e54478
Tanmay Khandelwal <tanmay.khandelwal@creativewebo.com>
2025-07-31 23:05:34 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+    return (
+        <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px[9vw]'>
+            <ToastContainer/>
+            <Navbar />
+            <SearchBar/>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/collection" element={<Collection />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/place-order" element={<PlaceOrder />} />
+                <Route path="/product/:productId" element={<Product />} />
+            </Routes>
+            <Footer />
+        </div>
+    )
+}
+
+export default App
