b87cce0cf3a0163c230e6e7f4ee567b21b5ddfac
Tanmay Khandelwal <tanmay.khandelwal@creativewebo.com>
2025-08-31 17:23:34 +0530

    mobile script fix for Ios2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import ShopContextProvider from './context/ShopContext.jsx';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleOAuthProvider } from '@react-oauth/google';
 import ScrollToTop from './components/ScrollTop.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    // <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    // </GoogleOAuthProvider>
         <BrowserRouter>
             <ShopContextProvider>
                  <ScrollToTop /> 
                 <App />
             </ShopContextProvider>
         </BrowserRouter>
-    </GoogleOAuthProvider>
 )
